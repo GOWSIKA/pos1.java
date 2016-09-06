fe0ec2691d85f3431f4059830f7e963f0bf98163
GOWSIKA <gowsikacse@gmail.com>
2016-09-06 09:26:53 +0530

    Create pos2.java

---
+++ pos2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class natural {
+
+	
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=0;
+		while(n>0)
+		{
+		int temp=n%10;
+		a=a+temp;
+		n=n/10;
+		}
+		System.out.println("Sum of Digit"+a);
+		
+	}
+
+}
